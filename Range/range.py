aa8f3912d587a9fcad4867f46f1a91bb59da62c5
dilshadgit <dilshad.abdulla@outlook.com>
2018-02-15 16:03:25 +0000

    Using nested loop for range and make small graph

---
+++ Range/range.py
@@ -0,0 +1,20 @@
+# start from 0 and ended at 9
+for x in range(0, 10):
+	print(x)
+
+
+for y in range(10, 21):
+	print(y)
+
+print('###################################')
+
+for x in range(4):
+	print('##')
+	for y in range(4):
+		print('==')
+		
+print('###################################')
+
+for x in range(0, 5):
+	for y in range(0, 5):
+		print(x, 'x', y, 'y')
